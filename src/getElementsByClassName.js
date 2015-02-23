b7aa5f0c747d529e72314b743e73c37561dc93f4
brantchoate <brantchoate@gmail.com>
2015-02-23 14:38:51 -0800

    finished getElementsByClassName spec

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,23 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+var getElementsByClassName = function(className){
+    var arr = [];
+    var iterate = function(node) {
+        if(node.nodeType === document.ELEMENT_NODE) {
+            if(node.className.indexOf(className) > -1) {
+                arr.push(node);
+            }
+            for (var i = 0; i < node.childNodes.length; i++) {
+                if(node.childNodes.length){
+                    iterate(node.childNodes[i]);
+                } else {
+                    return false;
+                }
+            }
+        }
+    };
+
+    iterate(document.body);
+    return arr;
 };
