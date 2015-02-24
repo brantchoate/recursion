a4be1f301d6e03ff4fb8299ca102e516876b3059
brantchoate <brantchoate@gmail.com>
2015-02-23 18:21:48 -0800

    made minor changes to style of classname + completed stringifyJSON

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,22 +5,22 @@
 
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className){
-    var arr = [];
-    var iterate = function(node) {
-        if(node.nodeType === document.ELEMENT_NODE) {
-            if(node.className.indexOf(className) > -1) {
-                arr.push(node);
-            }
-            for (var i = 0; i < node.childNodes.length; i++) {
-                if(node.childNodes.length){
-                    iterate(node.childNodes[i]);
-                } else {
-                    return false;
-                }
-            }
+  var arr = [];
+  var iterate = function(node) {
+    if(node.nodeType === document.ELEMENT_NODE) {
+      if(node.className.indexOf(className) > -1) {
+        arr.push(node);
+      }
+      for (var i = 0; i < node.childNodes.length; i++) {
+        if(node.childNodes.length){
+          iterate(node.childNodes[i]);
+        } else {
+          return false;
         }
-    };
+      }
+    }
+  };
 
-    iterate(document.body);
-    return arr;
+  iterate(document.body);
+  return arr;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,7 +4,48 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  if(typeof(obj) === 'function' || typeof(obj) === 'undefined'){
-    console.log(obj);
+  if(obj === null) {
+    return "null";
+  }
+
+  if(typeof(obj) === 'number' || typeof(obj) === 'boolean'){
+    return obj.toString();
+  }
+
+  if(obj.constructor === Object && typeof(obj) !== 'function') {
+    for(k in obj) {
+      if(typeof(obj[k]) === 'function') {
+        return '{}';
+      }
+    }
+
+    if(Object.keys(obj).length === 0) {
+      return '{}';
+    }
+
+    var objKeys = Object.keys(obj);
+
+    return '{' + 
+      objKeys.map(function(el, i){
+        var key = stringifyJSON(el);
+        var val = stringifyJSON(obj[el]);
+        return key +':' + val;
+      })
+  + '}';
+
+  }
+  if(typeof(obj) === 'string') {
+    return "\""+ obj + "\"";
+  }
+
+  if(obj.constructor === Array) {
+    for (var i = 0; i < obj.length; i++) {
+      if(obj.length){
+        obj[i] = stringifyJSON(obj[i]);
+      } else {
+        return false;
+      }
+    }
+    return "["+ obj + "]";
   }
 };
